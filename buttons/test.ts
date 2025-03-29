ca829b0b334d04337d2907b62ad92bfb668a81d7
7xr <55927013+7xrrr@users.noreply.github.com>
2025-03-29 20:34:48 +0100

    Delete buttons directory

--- buttons/test.ts
+++
@@ -1,9 +0,0 @@
-export default {
-    id: "", // button custom id here
-    permissions: [],
-    roleRequired: "",
-    function: async function ({ button }: { button: any }) {
-        const { client } = await import("../index.js");
-        button.reply("test");
-    },
-} as any;
