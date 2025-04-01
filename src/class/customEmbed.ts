696aefdbcc2fd1d4db1e7f5dea7bee4b657a2deb
7xr <55927013+7xrrr@users.noreply.github.com>
2025-04-01 01:54:03 +0100

    Delete src directory

--- src/class/customEmbed.ts
+++
@@ -1,9 +0,0 @@
-import { EmbedBuilder as builder } from "discord.js";
-import config from "../config";
-export class EmbedBuilder extends builder {
-    constructor() {
-        super();
-        this.setColor(`#${config.embedColor}`);
-
-    }
-}
--- src/contextMenus/test.ts
+++
@@ -1,8 +0,0 @@
-export default {
-    name: "test",
-    type: "message",
-    function: async function ({ interaction }: { interaction: any }) {
-        const { client } = await import("../index.js");
-        interaction.reply("test");
-    },
-} as any;
